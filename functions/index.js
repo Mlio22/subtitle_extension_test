8d5d2fcd3c5a4ecad29f7abf143a7a08c36d53bc
Muhammat Lio Pratama <muhammatliopratama@gmail.com>
2021-06-28 12:28:52 +0700

    added netlify-functions

--- functions/index.js
+++ functions/index.js
@@ -1,6 +1,19 @@
-exports.handler = (event, context, callback) => {
-  callback(null, {
-    statusCode: 200,
-    body: "hello world",
-  });
+const fetch = require("node-fetch");
+exports.handler = async (event, context) => {
+  const API_ENDPOINT = "http://54.169.236.48:5000/subtitles/a";
+
+  try {
+    const response = await fetch(API_ENDPOINT);
+    const data = await response.json();
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ data }),
+    };
+  } catch (error) {
+    console.log(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: "failed fetching data" }),
+    };
+  }
 };
